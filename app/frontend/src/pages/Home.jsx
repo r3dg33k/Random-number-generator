b4ab60a1018163d24b29994920f180e4a5351e83
r3dg33k <2913793+r3dg33k@users.noreply.github.com>
2026-01-13 21:01:45 +0300

    Delete app directory

--- app/frontend/src/pages/Home.jsx
+++
@@ -1,4 +0,0 @@
-                <CardTitle className=\"text-2xl\">Normal Mode</CardTitle>
-                <CardDescription className=\"text-base\">
-                  Generate two random numbers between 1-6 with a simple tap
-                </CardDescription>
